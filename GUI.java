ca5be505849074fd520cbb62dc1ae5a63eaab670
mathnerdrk <rahul.krishnan@hotmail.com>
2015-03-03 08:42:06 -0700

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -10,32 +10,32 @@ import java.util.*;
 public class GUI extends Canvas 
 {
     Random rand = new Random();
-    int[] places = new int[29]; //why 29?
+    int[] places = new int[29]; //why 29? what is this array for?
 
 	
 	
 	public GUI()
 	{
-        	int[] places = new int[29]
+        	int[] places = new int[29]; //why again???
         	for (int i = 15, l=0; i < 450; i+= 15, l++)
         	{
         	    places[l] = i;
-        	}// if "grid starts at (15, 15) and ends at (435, 435)
+        	}// if grid starts at (15, 15) and ends at (435, 435)
 		keyHandler listener = new keyHandler();
 		addKeyListener(listener);
 		setFocusable(true);
 	}
-
-    static Rectangle2D head = new Rectangle2D.Double(places[rand.nextInt(30)], places[rand.nextInt(30)], 15, 15);
-    static boolean onScreen[] = new boolean[900];
-    static Rectangle2D snake[] = new Rectangle2D[900];
+	//Note: we can't do it at a random coordinate unless we specify a random coordinate in the top left quarter (don't want player starting going off the screen right at the start)
+	static Rectangle2D head = new Rectangle2D.Double(places[rand.nextInt(30)], places[rand.nextInt(30)], 15, 15);
+	static boolean onScreen[] = new boolean[900];
+	static Rectangle2D snake[] = new Rectangle2D[900];
 	
 	public static void main(String[] args)
 	{
 		snake[0] = head;
 		onScreen[0] = true;
 		
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame("Snake!!!");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		Canvas canvas = new GUI();
