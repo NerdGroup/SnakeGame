de9603bfd1ce0add1df8018fd24b2b744c6e20b1
mathnerdrk <rahul.krishnan@hotmail.com>
2015-03-04 16:31:34 -0700

    Create rect.java

---
+++ rect.java
@@ -0,0 +1,10 @@
+//New class for creating Rectangles!
+package snake;
+
+import java.awt.geom.Rectangle2D;
+
+public class rect {
+	Rectangle2D rect;
+	String direction;
+	boolean onScreen = false;
+}
